449269d0b66c71708bc649ffea543784fcd26d4f
Kuberwastaken <Kuberhob@gmail.com>
2024-12-24 14:38:54 +0530

    Added more linux related commands

--- script.js
+++ script.js
@@ -81,11 +81,165 @@ function handleCommand(inputField, output, mainInfo) {
     case "cv":
       showResume(output);
       break;
-    default:
-      output.innerHTML += `<div>Not found</div>`;
-  }
+    case "misc commands":
+    case "misc":
+     output.innerHTML += `<div>This terminal (tries to) simulate a Linux shell with various commands. Try to find some easter_eggs.</div>`;
+       break;
+    case "ls":
+      output.innerHTML += `<div>projects  blog  github  linkedin  discord  youtube  resume  easter_eggs</div>`;
+      break;
+     case "cd easter_eggs":
+     case "easter_eggs":
+     case "ls easter_eggs":
+     case "cd misc_commands":
+     output.innerHTML += `
+     <div>
+       <pre style="white-space: pre-wrap; word-wrap: break-word;">
+         alias      cat        cd        chmod      curl      date      df
+         dmesg      echo       free      grep       gsown      history   hostname
+         kill       locate     lsblk      lsmod      man        mkdir     mount   
+         ping       ps         reboot     shutdown   tar        top        touch
+         uptime     unzip      wget       whoami     pwd
+       </pre>
+     </div>
+   `;   
+   case "pwd":
+    output.innerHTML += `<div>/home/kuber</div>`;
+    break;
+case "mkdir":
+case "cat":
+case "touch":
+case "man":
+    output.innerHTML += `<div>Command requires a file or directory name to perform the operation.</div>`;
+    break;
+case "man":
+    output.innerHTML += `<div>No manual entry for command.</div>`;
+    break;
+case "clearHistory":
+    output.innerHTML += `<div>History cleared.</div>`;
+    break;
+case "Sudo rm -rf":
+    output.innerHTML += `<div>Seriously?.</div>`;
+    break;
+case "shutdown":
+    output.innerHTML += `<div>Shutdown initiated. The system will power off shortly (not).</div>`;
+    break;
+case "reboot":
+    output.innerHTML += `<div>Rebooting... (Not really)</div>`;
+    break;
+case "date":
+    const currentDate = new Date();
+    const dateStr = currentDate.toLocaleString('en-IN', { weekday: 'short', year: 'numeric', month: 'short', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: false });
+    output.innerHTML += `<div>${dateStr}</div>`;
+    break;
+case "uptime":
+    output.innerHTML += `<div>up 6 hours, 30 minutes, 42 seconds</div>`;
+    break;
+case "whoami":
+    output.innerHTML += `<div>kuber's portfolio</div>`;
+    break;
+case "top":
+    output.innerHTML += `<div>Processes running: 50 (top is a real-time resource monitor)</div>`;
+    break;
+case "df":
+    output.innerHTML += `<div>Filesystem          1K-blocks     Used Available Use% Mounted on</div>
+    <div>/dev/sda1            51200000   23000000  28000000  45% /</div>`;
+    break;
+case "ps":
+    output.innerHTML += `<div>PID TTY      STAT   TIME COMMAND</div>
+    <div>1234 tty1     S      0:05 bash</div>
+    <div>1235 tty1     R      0:02 node</div>`;
+    break;
+case "kill":
+    output.innerHTML += `<div>Usage: kill [pid]</div>`;
+    break;
+case "ping":
+    if (args.length === 0) {
+        output.innerHTML += `<div>Pinging google.com with 32 bytes of data:</div>
+        <div>Reply from google.com: bytes=32 time=14ms TTL=56</div>`;
+    } else {
+        const domain = args[0];
+        output.innerHTML += `<div>Pinging ${domain} with 32 bytes of data:</div>
+        <div>Reply from ${domain}: bytes=32 time=14ms TTL=56</div>`;
+    }
+    break;
+case "curl":
+    output.innerHTML += `<div>curl: try 'curl --help' or 'curl --manual' for more information</div>`;
+    break;
+case "wget":
+    output.innerHTML += `<div>wget: command not found</div>`;
+    break;
+case "lsblk":
+    output.innerHTML += `<div>NAME   MAJ:MIN RM   SIZE RO TYPE MOUNTPOINT</div>
+    <div>sda      8:0    0   50G  0 disk /</div>
+    <div>sdb      8:16   0   50G  0 disk /data</div>`;
+    break;
+case "history":
+    const historyData = ["ls", "cd projects", "git status", "python3 -m http.server"];
+    historyData.forEach((cmd, index) => {
+        output.innerHTML += `<div>${index + 1}  ${cmd}</div>`;
+    });
+    break;
+case "echo":
+    if (args.length === 0) {
+        output.innerHTML += `<div>echo: command requires a string to output.</div>`;
+    } else {
+        output.innerHTML += `<div>${args.join(" ")}</div>`;
+    }
+    break;
+case "locate":
+    output.innerHTML += `<div>locate: command requires a search term.</div>`;
+    break;
+case "grep":
+    output.innerHTML += `<div>grep: command requires a pattern to search.</div>`;
+    break;
+case "chmod":
+    output.innerHTML += `<div>chmod: command requires a file and permissions.</div>`;
+    break;
+case "chown":
+    output.innerHTML += `<div>chown: command requires a user and a file.</div>`;
+    break;
+case "tar":
+    output.innerHTML += `<div>tar: command requires an archive to create or extract.</div>`;
+    break;
+case "gzip":
+    output.innerHTML += `<div>gzip: command requires a file to compress.</div>`;
+    break;
+case "unzip":
+    output.innerHTML += `<div>unzip: command requires a zip file to extract.</div>`;
+    break;
+case "alias":
+    output.innerHTML += <div>alias: command requires a name and value to assign.</div>;
+    break;
+case "sudo":
+    output.innerHTML += `<div>sudo: command requires administrative privileges.</div>`;
+    break;
+case "mount":
+    output.innerHTML += `<div>mount: command requires a filesystem to mount.</div>`;
+    break;
+case "umount":
+    output.innerHTML += `<div>umount: command requires a filesystem to unmount.</div>`;
+    break;
+case "hostname":
+    output.innerHTML += `<div>kubers-portfolio</div>`;
+    break;
+case "free":
+    output.innerHTML += `<div>Mem:   16GB  total,  2GB  used,  14GB  free,  500MB buffers</div>`;
+    break;
+case "lsmod":
+    output.innerHTML += `<div>Module                  Size  Used by</div>
+    <div>snd_hda_codec_realtek   106662  1</div>
+    <div>i2c_core               45600  1 i2c_hid</div>`;
+    break;
+case "dmesg":
+    output.innerHTML += `<div>[  123.456789] Initializing USB devices</div>
+    <div>[  123.789012] Network interface eth0: up</div>`;
+    break;
+default:
+    output.innerHTML += `<div>Command not found.</div>`;
+}
 
-  scrollToLatest(output);
+scrollToLatest(output);
 }
 
 /**
@@ -187,7 +341,8 @@ const helpCmd = `
   [<span class="commandName">projects</span>] or [<span class="commandName">pj</span>]<br />
   [<span class="commandName">resume</span>] or [<span class="commandName">cv</span>]<br />
   [<span class="commandName">blog</span>]<br />
-  [<span class="commandName">neofetch</span>]<br /><br />
+  [<span class="commandName">neofetch</span>]<br />
+  [<span class="commandName">misc commands</span>]<br /><br />
   [<span class="commandName">clear</span>]<br /><br />
   Contact me: <br />
   
@@ -282,4 +437,3 @@ let projectCmd = `
     </article>
   </div>
 `;
-
