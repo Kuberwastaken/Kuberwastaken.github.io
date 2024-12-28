fa6c31a5dce8a8862da7da87bc808c2ac9307e4b
Kuberwastaken <Kuberhob@gmail.com>
2024-12-29 02:29:05 +0530

    Fixed Skillbar

--- script.js
+++ script.js
@@ -359,47 +359,47 @@ const skillsBar = `
   <div class="container">
     <div class="flex">
       <h2>HTML/EJS:</h2>
-      <div class="skillBar"><div class="skillBarItem1" style="width: 70%"></div></div>
+      <div class="skillBar"><div class="skillBarItem skillBarItem1"></div></div>
       <h3>70%</h3>
     </div>
     <div class="flex">
       <h2>CSS/SCSS:</h2>
-      <div class="skillBar"><div class="skillBarItem2"></div></div>
+      <div class="skillBar"><div class="skillBarItem skillBarItem2"></div></div>
       <h3>100%</h3>
     </div>
     <div class="flex">
       <h2>JS:</h2>
-      <div class="skillBar"><div class="skillBarItem3" style="width: 60%"></div></div>
+      <div class="skillBar"><div class="skillBarItem skillBarItem3"></div></div>
       <h3>60%</h3>
     </div>
     <div class="flex">
       <h2>TS:</h2>
-      <div class="skillBar"><div class="skillBarItem4" style="width: 60%"></div></div>
+      <div class="skillBar"><div class="skillBarItem skillBarItem4"></div></div>
       <h3>55%</h3>
     </div>
     <div class="flex">
       <h2>NODE.JS:</h2>
-      <div class="skillBar"><div class="skillBarItem5"></div></div>
+      <div class="skillBar"><div class="skillBarItem skillBarItem5"></div></div>
       <h3>75%</h3>
     </div>
     <div class="flex">
       <h2>REACT.JS:</h2>
-      <div class="skillBar"><div class="skillBarItem6" style="width: 75%"></div></div>
+      <div class="skillBar"><div class="skillBarItem skillBarItem6"></div></div>
       <h3>75%</h3>
     </div>
     <div class="flex">
       <h2>RUST:</h2>
-      <div class="skillBar"><div class="skillBarItem7" style="width: 10%"></div></div>
+      <div class="skillBar"><div class="skillBarItem skillBarItem7"></div></div>
       <h3>10%</h3>
     </div>
     <div class="flex">
       <h2>PYTHON:</h2>
-      <div class="skillBar"><div class="skillBarItem8" style="width: 65%"></div></div>
+      <div class="skillBar"><div class="skillBarItem skillBarItem8"></div></div>
       <h3>65%</h3>
-    <div>
+    </div>
     <div class="flex">
       <h2>C:</h2>
-      <div class="skillBar"><div class="skillBarItem9" style="width: 75%"></div></div>
+      <div class="skillBar"><div class="skillBarItem skillBarItem9"></div></div>
       <h3>75%</h3>
     </div>
   </div>
