b92401b5ab4e64b9164ec68ad5bc59405c1a431e
Kuberwastaken <Kuberhob@gmail.com>
2025-02-17 18:53:02 +0530

    Finally updated Skills

--- src/constants/skillsBar.js
+++ src/constants/skillsBar.js
@@ -4,15 +4,6 @@ const skillsBar = `
   
   <div style="display: flex; flex-wrap: wrap; gap: 20px;">
     <div style="flex: 1 1 30%;">
-      <div>BigQuery</div>
-      <div class="skillBar">
-        <div class="skillBarItem" style="--width: 80%;">
-          <span style="margin-left: 10px; color: #ffebcd;">80%</span>
-        </div>
-      </div>
-    </div>
-
-    <div style="flex: 1 1 30%;">
       <div>C</div>
       <div class="skillBar">
         <div class="skillBarItem" style="--width: 70%;">
@@ -24,33 +15,6 @@ const skillsBar = `
     <div style="flex: 1 1 30%;">
       <div>CSS</div>
       <div class="skillBar">
-        <div class="skillBarItem" style="--width: 85%;">
-          <span style="margin-left: 10px; color: #ffebcd;">85%</span>
-        </div>
-      </div>
-    </div>
-
-    <div style="flex: 1 1 30%;">
-      <div>Flask</div>
-      <div class="skillBar">
-        <div class="skillBarItem" style="--width: 85%;">
-          <span style="margin-left: 10px; color: #ffebcd;">85%</span>
-        </div>
-      </div>
-    </div>
-
-    <div style="flex: 1 1 30%;">
-      <div>HTML</div>
-      <div class="skillBar">
-        <div class="skillBarItem" style="--width: 70%;">
-          <span style="margin-left: 10px; color: #ffebcd;">70%</span>
-        </div>
-      </div>
-    </div>
-
-    <div style="flex: 1 1 30%;">
-      <div>Javascript</div>
-      <div class="skillBar">
         <div class="skillBarItem" style="--width: 65%;">
           <span style="margin-left: 10px; color: #ffebcd;">65%</span>
         </div>
@@ -58,16 +22,79 @@ const skillsBar = `
     </div>
 
     <div style="flex: 1 1 30%;">
-      <div>Node</div>
+      <div>Flask</div>
       <div class="skillBar">
-        <div class="skillBarItem" style="--width: 70%;">
-          <span style="margin-left: 10px; color: #ffebcd;">70%</span>
+        <div class="skillBarItem" style="--width: 69%;">
+          <span style="margin-left: 10px; color: #ffebcd;">69%</span>
+        </div>
+      </div>
+    </div>
+
+    <div style="flex: 1 1 30%;">
+      <div>Git</div>
+      <div class="skillBar">
+        <div class="skillBarItem" style="--width: 75%;">
+          <span style="margin-left: 10px; color: #ffebcd;">75%</span>
+        </div>
+      </div>
+    </div>
+
+    <div style="flex: 1 1 30%;">
+      <div>Gradio</div>
+      <div class="skillBar">
+        <div class="skillBarItem" style="--width: 90%;">
+          <span style="margin-left: 10px; color: #ffebcd;">90%</span>
+        </div>
+      </div>
+    </div>
+
+    <div style="flex: 1 1 30%;">
+      <div>HTML</div>
+      <div class="skillBar">
+        <div class="skillBarItem" style="--width: 69%;">
+          <span style="margin-left: 10px; color: #ffebcd;">69%</span>
+        </div>
+      </div>
+    </div>
+
+    <div style="flex: 1 1 30%;">
+      <div>JavaScript</div>
+      <div class="skillBar">
+        <div class="skillBarItem" style="--width: 60%;">
+          <span style="margin-left: 10px; color: #ffebcd;">60%</span>
+        </div>
+      </div>
+    </div>
+
+    <div style="flex: 1 1 30%;">
+      <div>LaTeX</div>
+      <div class="skillBar">
+        <div class="skillBarItem" style="--width: 80%;">
+          <span style="margin-left: 10px; color: #ffebcd;">80%</span>
+        </div>
+      </div>
+    </div>
+
+    <div style="flex: 1 1 30%;">
+      <div>Node.js</div>
+      <div class="skillBar">
+        <div class="skillBarItem" style="--width: 60%;">
+          <span style="margin-left: 10px; color: #ffebcd;">60%</span>
         </div>
       </div>
     </div>
 
     <div style="flex: 1 1 30%;">
       <div>Python</div>
+      <div class="skillBar">
+        <div class="skillBarItem" style="--width: 75%;">
+          <span style="margin-left: 10px; color: #ffebcd;">75%</span>
+        </div>
+      </div>
+    </div>
+
+    <div style="flex: 1 1 30%;">
+      <div>R</div>
       <div class="skillBar">
         <div class="skillBarItem" style="--width: 80%;">
           <span style="margin-left: 10px; color: #ffebcd;">80%</span>
@@ -78,8 +105,8 @@ const skillsBar = `
     <div style="flex: 1 1 30%;">
       <div>React</div>
       <div class="skillBar">
-        <div class="skillBarItem" style="--width: 85%;">
-          <span style="margin-left: 10px; color: #ffebcd;">85%</span>
+        <div class="skillBarItem" style="--width: 60%;">
+          <span style="margin-left: 10px; color: #ffebcd;">60%</span>
         </div>
       </div>
     </div>
@@ -87,8 +114,8 @@ const skillsBar = `
     <div style="flex: 1 1 30%;">
       <div>SQL</div>
       <div class="skillBar">
-        <div class="skillBarItem" style="--width: 75%;">
-          <span style="margin-left: 10px; color: #ffebcd;">75%</span>
+        <div class="skillBarItem" style="--width: 85%;">
+          <span style="margin-left: 10px; color: #ffebcd;">85%</span>
         </div>
       </div>
     </div>
@@ -96,8 +123,8 @@ const skillsBar = `
     <div style="flex: 1 1 30%;">
       <div>Tableau</div>
       <div class="skillBar">
-        <div class="skillBarItem" style="--width: 60%;">
-          <span style="margin-left: 10px; color: #ffebcd;">60%</span>
+        <div class="skillBarItem" style="--width: 80%;">
+          <span style="margin-left: 10px; color: #ffebcd;">80%</span>
         </div>
       </div>
     </div>
